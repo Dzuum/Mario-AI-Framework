f8344eaef884f9d421d893967361a51c998caccc
Joona Luodonpää <joona.luodonpaa@vividworks.com>
2023-04-13 20:02:57 +0300

    Deserialize the stored Patterns from disk when generating patterns

--- src/custom/LevelGenerator.java
+++ src/custom/LevelGenerator.java
@@ -1,8 +1,11 @@
 package custom;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,52 +15,64 @@ import engine.core.MarioTimer;
 
 public class LevelGenerator implements MarioLevelGenerator {
     private int patternCount;
-    private String folderName = "results/lvl-1/";
+    private String sourceLevel = "World 1-1";
 
     private Random rand;
+    private List<Pattern> patterns;
 
     public LevelGenerator(int patternCount) {
         this.patternCount = patternCount;
-    }
-
-    /*
-     * Select a random pattern file and return its lines.
-     */
-    private List<String> getRandomPattern() throws IOException {
-        File[] listOfFiles = new File(folderName).listFiles();
-        List<String> lines = Files.readAllLines(listOfFiles[rand.nextInt(listOfFiles.length)].toPath());
-        return lines;
+        patterns = new ArrayList<>();
     }
 
     @Override
     public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer) {
+        loadPatterns(sourceLevel);
+
         rand = new Random();
         model.clearMap();
 
         // The start x index for the next pattern
         int currentMapX = 0;
 
-        try {
-            for (int i = 0; i < patternCount; i++) {
-                List<String> pattern = getRandomPattern();
+        for (int i = 0; i < patternCount; i++) {
+            Pattern pattern = patterns.get(rand.nextInt(patterns.size()));
+            List<String> geometry = pattern.getGeometry();
 
-                // length() should be OK since Files.readAllLines seems to omit newline characters
-                int patternWidth = pattern.get(0).length();
-
-                String combinedPattern = "";
-                for (int j = 0; j < pattern.size(); j++) {
-                    combinedPattern += pattern.get(j) + "\n";
-                }
-
-                model.copyFromString(currentMapX, 0, 0, 0, patternWidth, model.getHeight(), combinedPattern);
-
-                currentMapX += patternWidth;
+            String combinedPattern = "";
+            for (int j = 0; j < geometry.size(); j++) {
+                combinedPattern += geometry.get(j) + "\n";
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            model.copyFromString(currentMapX, 0, 0, 0, pattern.getTileWidth(), model.getHeight(), combinedPattern);
+
+            currentMapX += pattern.getTileWidth();
         }
 
         return model.getMap();
+    }
+
+    private void loadPatterns(String level) {
+        try {
+            Path directoryPath = Paths.get(Settings.RESULTS_FOLDER_NAME, level, Settings.PATTERNS_FOLDER_NAME);
+            File directory = new File(directoryPath.toString());
+            File[] directoryListing = directory.listFiles();
+
+            for (File file : directoryListing) {
+                if (file.getName().startsWith(Settings.PATTERNS_FILE_NAME)) {
+                    FileInputStream fileStream = new FileInputStream(file.getPath());
+                    ObjectInputStream objectStream = new ObjectInputStream(fileStream);
+                        
+                    patterns.add((Pattern)objectStream.readObject());
+                        
+                    objectStream.close();
+                    fileStream.close();
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Error reading patterns for level " + level);
+            ex.printStackTrace();
+        }
     }
 
     @Override
